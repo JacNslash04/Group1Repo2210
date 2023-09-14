3a173fc1136eec246fbce4d7c586cb373a0fab8c
DroopyMantis <julian.mazzier@gmail.com>
2023-09-13 18:12:52 -0700

    commiting
    -m nato alphabet

---
+++ Classwork/Classwork3/NatoPhoneticAlphabet.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main( int argc, char * argv[] ) {
+    printf("Enter Word : \n");
+    char str[100];
+    int len = 0;
+    fgets(str, 100, stdin);
+    for (int i = 0; str[i] != '\0'; ++i){
+        len++;
+    }
+    printf("\n");
+
+    int j =0;
+    char ch;
+    while (str[j]) { // makes each char in our string lowercase
+        ch = str[j];
+        putchar(tolower(ch));
+        j++;
+    }
+
+    char nato[26][10] = { "alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel",
+                         "india", "juliett", "kilo", "lima", "mike", "november", "oscar", "papa",
+                         "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey",
+                         "xray", "yankee", "zulu" };
+
+    char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
+    for(int i = 0; i <= len; i++){ // nested forloop iterates through each char of string and prints correlating nato element
+        for(int j = 0; j < 26; j++){
+            if(str[i] == alphabet[j]){
+                printf("\n");
+                printf("%s", nato[j] );
+            }
+        }
+    }
+    return 0;
+}
