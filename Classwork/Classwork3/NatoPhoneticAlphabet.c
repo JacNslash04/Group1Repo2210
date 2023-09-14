bcc0e3a1d65741d74d8c308ff4cca50fd7450f69
DroopyMantis <julian.mazzier@gmail.com>
2023-09-13 18:14:40 -0700

    commit

--- Classwork/Classwork3/NatoPhoneticAlphabet.c
+++ Classwork/Classwork3/NatoPhoneticAlphabet.c
@@ -2,11 +2,11 @@
 #include <ctype.h>
 
 int main( int argc, char * argv[] ) {
-    printf("Enter Word : \n");
-    char str[100];
+    printf("Enter Word or Phrase: \n");
+    char str[1000];
     int len = 0;
-    fgets(str, 100, stdin);
-    for (int i = 0; str[i] != '\0'; ++i){
+    fgets(str, 1000, stdin);
+    for (int i = 0; str[i] != '\0'; ++i){ // gets length of string
         len++;
     }
     printf("\n");
