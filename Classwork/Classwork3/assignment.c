e04894606e7dd0814bd6273934d984f0df09c462
Jacob Mendoza <112521288+JacNslash04@users.noreply.github.com>
2023-10-11 17:00:19 -0700

    Adding place to submit Classwork 7

---
+++ Classwork/Classwork3/assignment.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+
+int main( int argc, char * argv[])
+{
+    char NATO[26][10] = { 'alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india', 'juliet', 'kilo', 'lima', 'mike', 
+'november', 'oscar', 'papa', 'quebec', 'romeo', 'sierra', 'tango', 'uniform', 'victor', 'whiskey','xray', 'yankee', 'zulu'};
+
+    char ALPHABET [26] = 'abcdefghijklmnopqrstuvwxyz';
+
+    char user_input; 
+
+    printf("Enter a word to be converted into the NATO alphabet: \n ");
+    scanf("%d", user_input);
+
+   
+}
+
+
